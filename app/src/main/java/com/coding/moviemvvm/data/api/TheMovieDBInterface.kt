4f9b42f020c85ea9f25c22628d7e6f4ce3080663
Emad Shamaki <emad.shamsaki@studenti.unipd.it>
2022-02-13 09:17:33 +0100

    add recyclerview

--- app/src/main/java/com/coding/moviemvvm/data/api/TheMovieDBInterface.kt
+++ app/src/main/java/com/coding/moviemvvm/data/api/TheMovieDBInterface.kt
@@ -2,9 +2,11 @@ package com.coding.moviemvvm.data.api
 
 import com.bumptech.glide.request.SingleRequest
 import com.coding.moviemvvm.data.vo.MovieDetails
+import com.coding.moviemvvm.data.vo.MovieResponse
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface TheMovieDBInterface {
 
@@ -12,6 +14,9 @@ interface TheMovieDBInterface {
 // https://api.themoviedb.org/3/movie/299534?api_key=6e63c2317fbe963d76c3bdc2b785f6d1
 // https://image.tmdb.org/t/p/w342/or06FN3Dka5tukK1e9sl16pB3iy.jpg
 
+    @GET("movie/popular")
+    fun getPopularMovie(@Query("page") page: Int): Single<MovieResponse>
+
     @GET("movie/{movie_id}")
-    fun getMovieDetails(@Path("movie_id") id:Int) : Single<MovieDetails>
+    fun getMovieDetails(@Path("movie_id") id: Int): Single<MovieDetails>
 }
